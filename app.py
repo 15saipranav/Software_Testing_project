2eb926b48d4b8f280913985b590d24c47911dce1
pramodsai2000 <kondamaripramodsai@gmail.com>
2021-03-13 17:48:44 +0100

    Fail Tests

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import math
+#This is a simple calculator program
 class Application:
     def add(self,a,b):
+        """This is addition method"""
         return 0
     def sub(self,a,b):
+        """This is subtraction method"""
         return 0
     def mul(self,a,b):
+        """This is multiplication method"""
         return 0
     def div(self,a,b):
+        """This is division method"""
         return 0
     def sqrt(self,val):
+        """This is square root method"""
         return 0
     def log(self,val):
+        """This is logarithm method"""
         return 0
     def exp(self,base,power):
+        """This is exponential method"""
         return 0
